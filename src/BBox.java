f6de3308b2bd94e1cceaaf2ecb5e6c1a38f77fb4
omi7894 <omi7894@naver.com>
2020-07-14 17:49:34 +0900

    .

---
+++ src/BBox.java
@@ -0,0 +1,58 @@
+public class BBox {
+
+	private double minX;
+	private double minY;
+	private double maxX;
+	private double maxY;
+
+	public BBox() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public BBox(double minX, double minY, double maxX, double maxY) {
+		super();
+		this.minX = minX;
+		this.minY = minY;
+		this.maxX = maxX;
+		this.maxY = maxY;
+	}
+
+	public double getMinX() {
+		return minX;
+	}
+
+	public void setMinX(double minX) {
+		this.minX = minX;
+	}
+
+	public double getMinY() {
+		return minY;
+	}
+
+	public void setMinY(double minY) {
+		this.minY = minY;
+	}
+
+	public double getMaxX() {
+		return maxX;
+	}
+
+	public void setMaxX(double maxX) {
+		this.maxX = maxX;
+	}
+
+	public double getMaxY() {
+		return maxY;
+	}
+
+	public void setMaxY(double maxY) {
+		this.maxY = maxY;
+	}
+	
+	
+	
+	
+	
+	
+}
